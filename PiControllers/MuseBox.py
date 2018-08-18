1b9485d25bb92ef41d0d442b38ce714defef0fda
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-08-18 19:24:27 -0400

    RPi controlled error light.

--- PiControllers/MuseBox.py
+++ PiControllers/MuseBox.py
@@ -4,40 +4,48 @@ import time
 from MusicController import MusicController
 from SignalParser import SignalParser
 from IRDecoder import IRDecoder
+from LEDController import LEDController
 
 ser=serial.Serial("/dev/ttyACM0", 28800, timeout=0.05);
 time.sleep(1)
 ser.baudrate=28800;
 VERSION = "0.0.1"
-print("Running MuseBox, version: " + VERSION + "...");
 
-GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(11, GPIO.OUT)
+print("Running MuseBox, version: " + VERSION + "...");
 
 class MuseBox():
 	def __init__(self):
 		self.power = True
 		self.decoder = IRDecoder()
 		self.parser = SignalParser(ser)
-		self.controller = MusicController()
+		self.music_controller = MusicController()
+		self.led_controller = LEDController()
 	
 	def toggle_power(self):
 		self.power = not self.power
 		exit(1)
+
+
 	
 	def process_loop(self):
 		while (self.power):
-			serial_line_result = ser.read_until(b'70')
-			ser.flushInput()
-			sig_recv = self.decoder.decode(serial_line_result, True)
-			parse_result = self.parser.parse(sig_recv)
-			if (parse_result == 'p'):
-				self.controller.toggle_start_stop()
-			if (parse_result == 'n'):
-				self.controller.play_new_song()
-			elif (parse_result == 'x'):
-				self.toggle_power()
+			if (ser.inWaiting()):
+				serial_line_result = ser.read_until(b'70')
+				ser.flushInput()
+				sig_recv = self.decoder.decode(serial_line_result, True)
+				if (self.parser.is_valid(sig_recv)):
+					parse_result = self.parser.parse(sig_recv)
+					if (parse_result == 'p'):
+						self.led_controller.blink_processing_light()
+						self.music_controller.toggle_start_stop()
+					if (parse_result == 'n'):
+						self.led_controller.blink_processing_light()
+						self.music_controller.play_new_song()
+					elif (parse_result == 'x'):
+						self.led_controller.blink_processing_light()
+						self.toggle_power()
+				else:
+					self.led_controller.blink_error_light()
 			time.sleep(2)
 				
 box1 = MuseBox()
