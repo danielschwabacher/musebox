ee55c7cb7341e266fa6c421cbddf1c8fe9328ed0
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-08-18 19:24:59 -0400

    fixed lighting issue when skipping songs.

--- PiControllers/MusicController.py
+++ PiControllers/MusicController.py
@@ -10,18 +10,32 @@ class MusicController():
 		pygame.mixer.init()
 		
 	def toggle_start_stop(self):
-		if (not self.has_music_loaded):
+		'''
+			Cases:
+			1.) music loaded and playing
+			2.) music loaded and not playing
+			3.) music not loaded and playing -- impossible
+			4.) music not loaded and not playing
+		'''
+		if (self.has_music_loaded):
+			if (self.is_playing):
+				# Pause music
+				print("Pausing music...")
+				self.mixer_context.pause()
+				self.is_playing = False
+			else:
+				print("Resuming music...")
+				self.mixer_context.unpause()
+				self.is_playing = True
+		else:
 			print("Loading music...")
 			song_to_play = self.queue.get_random_song()
 			self.mixer_context.load(song_to_play)
-		if (self.is_playing):
-			print("Stopping music...")
-			self.mixer_context.stop()
-		else:
+			self.has_music_loaded = True
 			print("Playing music...")
 			self.mixer_context.play()
-		self.is_playing = not (self.is_playing)
-		self.has_music_loaded = True
+			self.is_playing = True
+			
 	
 	def play_new_song(self):
 		if (not self.is_playing):
