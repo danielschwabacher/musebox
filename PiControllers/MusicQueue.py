5c8f71cba8c32095034a79697613d2f78017a7c0
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-08-18 20:00:38 -0400

    primative music queue implementation. No longer replays songs already played.

--- PiControllers/MusicQueue.py
+++ PiControllers/MusicQueue.py
@@ -4,9 +4,22 @@ class MusicQueue():
 	def __init__(self):
 		self.home_dir = os.path.expanduser("~")
 		self.music_dir = self.home_dir + "/CranberryMusic/mp3s/"
+		self.played_songs = []
+		self.number_songs_in_dir = len(os.listdir(self.music_dir))
+	
+	def reset_songs(self):
+		self.played_songs = []
+			
 	def get_random_song(self):
+		if (self.number_songs_in_dir == len(self.played_songs)):
+			self.reset_songs()
 		random_file = random.choice(os.listdir(self.music_dir))
-		path = os.path.join(self.music_dir, random_file)
-		return path
-		
+		if (random_file not in self.played_songs):
+			print("Playing song: " + str(random_file))
+			self.played_songs.append(random_file)
+			path = os.path.join(self.music_dir, random_file)
+			return path
+		else:
+			return self.get_random_song()
+			
 		
