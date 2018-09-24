8eaeadd8360e33bdb8644fa2e55cf84abda68fc7
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-09-23 20:50:04 -0400

    made ir codes configurable via JSON property files.

--- PiControllers/SignalParser.py
+++ PiControllers/SignalParser.py
@@ -1,11 +1,26 @@
+import json
+import os
+
 class SignalParser():
 	'''
 		SignalParser objects handle translating IR codes.
 	'''
 	def __init__(self, serial_obj):
 		self.serial = serial_obj
-		self.valid_codes = [b'fd', b'3d', b'5d', b'4f', b'9d', b'57', b'dd', b'1f']
+		self.prop_file = "led_remote.json"
+		self.properties_file_location = os.path.expanduser("~") + "/IrProperties/" + self.prop_file
+		self.valid_codes = []
+		self.lookup_map = {}
+		self.get_codes()
 		
+		
+	def get_codes(self):
+		with open(self.properties_file_location) as properties:
+			data = json.loads(properties.read())
+			for key, val in data.items():
+				self.valid_codes.append(val)
+				self.lookup_map[val] = key
+			
 	def is_valid(self, sig_code):
 		if (sig_code in self.valid_codes):
 			return True
@@ -23,74 +38,38 @@ class SignalParser():
 			This is essentially a lookup table which maps:
 			IR bytes -> Action characters
 		'''
-		if (signal_code == b'fd'):
-			'''
-				Represents the Start/Stop button 
-				on the IR remote
-			'''
-			rv = 'p'
-			self.serial.write(b'p')
-			return rv
-			
-			
-		if (signal_code == b'3d'):
-			'''
-				Represents the next button on 
-				the IR remote
-			'''
-			rv = 'n'
-			self.serial.write(b'n')
-			return rv
-
-			
-			
-		if (signal_code == b'5d'):
-			'''
-				Represents the Power button on 
-				the IR remote
-			'''
-			rv = 'x'
-			self.serial.write(b'x')
-			return rv
-			
-		if (signal_code == b'4f'):
-			'''
-				Represnt the ST/REPT button on
-				the IR remote
-				Resets the music queue.
-				NOT USED
-			'''
-			rv = 'v'
-			return rv
+		if (signal_code in self.valid_codes):
+			try:
+				action = self.lookup_map[signal_code]
+				print("action is: " + action)
+				if (action == "power"):
+					rv = 'x'
+					self.serial.write(b'x')
+					return rv
+				if (action == "start_stop"):
+					rv = 'p'
+					self.serial.write(b'p')
+					return rv
+				if (action == "next_song"):
+					rv = 'n'
+					self.serial.write(b'n')
+					return rv		
+				if (action == "volume_up"):
+					rv = 'u'
+					return rv
+				if (action == "volume_down"):
+					rv = 'd'
+					return rv
+				if (action == "rewind"):
+					rv = 'r'
+					return rv				
+				if (action == "previous_song"):
+					rv = 'b'
+					return rv
+			except KeyError:
+				print("Key not found")
+				return '0'
+		else:
+			print("Code not found in valid codes.")
+			return '0'
 		
-		if (signal_code == b'9d'):
-			'''
-				Represents the volume up 
-				button on IR remote
-			'''
-			rv = 'u'
-			return rv
-			
-		if (signal_code == b'57'):
-			'''
-				Represents the volume down button on 
-				the IR remote
-			'''
-			rv = 'd'
-			return rv
-			
-		if (signal_code == b'dd'):
-			'''
-			Represents the back/rewind button on
-			the IR remote
-			'''
-			rv = 'r'
-			return rv
-			
-		if (signal_code == b'1f'):
-			'''
-				Represents the down button on the IR remote
-				Used to play previous song in queue.
-			'''
-			rv = 'b'
-			return rv 
